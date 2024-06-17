9d7c9b4b5f7f002a055aaf6bdc3b60b2f07b355b
Yusuke Fukumoto <ysk2645j4@gmail.com>
2024-06-17 21:26:22 +0900

    vanilla js で書き換え。データをモジュール化

--- js/script.js
+++ js/script.js
@@ -1,29 +1,41 @@
-$(() => {
-	let item = $('.grid-item'); //各写真をjQueryオブジェクトとして取得
-	let modal = document.querySelector('.modal'); //dialogタグ要素を取得
-	let content = $('.modal-content'); //モーダルの内容を表示する箇所をjQueryオブジェクトとして取得
+import { puppyArray } from './puppyArray.js';
+const gridContainer = document.querySelector('.grid-container');
+const modal = document.querySelector('.modal');
+const modalImg = document.createElement('img');
+const modalMsg = document.createElement('p');
 
-	//↓↓写真がクリックされたときの処理↓↓
-	item.on('click', function () {
-		let imgId = $(this).attr('id').slice(4); //クリックされた写真のidを取得して5文字目以降を取得
-		let caption = $(this).find('figcaption').text();//クリックされた写真内のキャプションを取得
-		scrollTop = $(window).scrollTop(); //現在のスクロール位置を取得(グローバル変数)
-		content.html(`<img src="img/image${imgId}.jpg"><p>${caption}</p>`); //モーダルの内容に写真とキャプションを挿入
-		$('body').css({
-			'position': 'fixed',
-			'top': `-${scrollTop}px`
-		}); //モーダルの背面がスクロールされないようにする & クリックイベント発生時のスクロール位置まで移動させる
-		modal.showModal(); //モーダルを表示させる(dialogタグ用のメソッド)
-		return false;
+puppyArray.forEach(puppy => {
+	const gridItem = document.createElement('li');
+	gridItem.classList.add('grid-item');
+
+	const gridInner = document.createElement('a');
+	gridInner.setAttribute('href', '#');
+	gridInner.setAttribute('data-msg', puppy.msg);
+
+	const gridImg = document.createElement('img');
+	gridImg.setAttribute('src', 'img/' + puppy.image);
+	gridImg.setAttribute('alt', puppy.alt);
+
+	gridInner.append(gridImg);
+	gridItem.append(gridInner);
+	gridContainer.append(gridItem);
+
+	gridInner.addEventListener('click', (e) => {
+		e.preventDefault();
+		modalImg.setAttribute('src', 'img/' + puppy.image);
+		modalImg.setAttribute('alt', puppy.alt);
+		modalMsg.innerText = puppy.msg;
+		modal.children[0].append(modalImg);
+		modal.children[0].append(modalMsg);
+
+		modal.showModal();
+		document.documentElement.style.overflow = 'hidden';
 	});
 
-	// ↓↓ モーダルを閉じる処理 ↓↓
-	$(document).on('click', function (e) {
-		if ($(e.target).attr('class') === 'modal') { //クリックされた要素のクラスがmodalだったとき
-			$('body').css({ 'position': '', 'top': '' }); //bodyをスクロールできるようにする
-			$(window).scrollTop(scrollTop); //写真がクリックされた時のスクロール位置まで移動
-			modal.close(); //モーダルを非表示にする(dialogタグ用のメソッド)
+	modal.addEventListener('click', (e) => {
+		if (e.target.closest('.modal-content') === null) {
+			modal.close();
+			document.documentElement.style.overflow = 'auto';
 		}
-		return false;
-	});
+	})
 });
